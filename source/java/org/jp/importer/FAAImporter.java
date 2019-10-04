61c43a3166f258bb11c7fb966c0de9a7188022a3
John Perry <johnperry@dls.net>
2019-10-04 14:09:33 -0500

    Create version date file
    Signed-off-by: John Perry <johnperry@dls.net>

--- source/java/org/jp/importer/FAAImporter.java
+++ source/java/org/jp/importer/FAAImporter.java
@@ -28,15 +28,21 @@ public class FAAImporter extends JFrame {
     File navFile = new File("NAV.txt");
     File twrFile = new File("TWR.txt");
     File awosFile = new File("AWOS.txt");
-    File airFile = new File("FAAFiles/Airports.txt");
-    File seaFile = new File("FAAFiles/Seaports.txt");
-    File ndbFile = new File("FAAFiles/NDBs.txt");
-    File vorFile = new File("FAAFiles/VORs.txt");
-    File xmlFile = new File("Airports.xml");
+    File fixFile = new File("FIX.txt");
+    File airFile = new File("APPFiles/Airports.txt");
+    File seaFile = new File("APPFiles/Seaports.txt");
+    File ndbFile = new File("APPFiles/NDBs.txt");
+    File vorFile = new File("APPFiles/VORs.txt");
+    File fxxFile = new File("APPFiles/Fixes.txt");
+    File xmlFile = new File("APPFiles/Airports.xml");
+    File verFile = new File("APPFiles/Version.txt");
+    
+    File assets = new File("/Development/Android/Airports/app/src/main/assets");
     
 	Hashtable<String,Airport> aptIndex = null;
 	Hashtable<String,Airport> aptTable = null;
 	Hashtable<String,NAV> navIndex = null;
+	Hashtable<String,Fix> fixIndex = null;
 	
 	public static void main(String args[]) {
 		new FAAImporter();
@@ -59,7 +65,9 @@ public class FAAImporter extends JFrame {
 			zob = new ZipObject(faaZipFile);
 			cpProgress.println("Creating the FAAFiles directory");
 			File faaFiles = new File("FAAFiles");
+			File appFiles = new File("APPFiles");
 			faaFiles.mkdirs();
+			appFiles.mkdirs();
 			
 			cpProgress.print("Expanding the APT.txt file ");
 			File aptFile = zob.extractFile(zob.getEntry("APT.txt"), faaFiles);
@@ -76,6 +84,10 @@ public class FAAImporter extends JFrame {
 			cpProgress.print("Expanding the AWOS.txt file ");
 			File awosFile = zob.extractFile(zob.getEntry("AWOS.txt"), faaFiles);
 			cpProgress.println(awosFile.getAbsolutePath());
+			
+			cpProgress.print("Expanding the FIX.txt file ");
+			File fixFile = zob.extractFile(zob.getEntry("FIX.txt"), faaFiles);
+			cpProgress.println(fixFile.getAbsolutePath());
 			
 			cpProgress.print("Expanding the README.txt file ");
 			File readMe = zob.extractFile(zob.getEntry("README.txt"), faaFiles);
@@ -165,6 +177,7 @@ public class FAAImporter extends JFrame {
 			text = text.replace("    ", " ");
 			FileUtil.setText(xmlFile, text);
 
+			//Get the Navaids
 			count = 0;
 			navIndex = new Hashtable<String,NAV>();
 			cpProgress.println("Processing the NAV.txt file");
@@ -190,6 +203,31 @@ public class FAAImporter extends JFrame {
 			cpProgress.println("Writing the VORs.txt file");
 			text = getNavText(navs, "VO");
 			FileUtil.setText(vorFile, text);
+			
+			//Get the fixes
+			count = 0;
+			fixIndex = new Hashtable<String,Fix>();
+			cpProgress.println("Processing the FIX.txt file");
+			reader = new BufferedReader(new FileReader(fixFile));
+			while ( (line = reader.readLine()) != null ) {
+				if (line.startsWith("FIX1")) {
+					Fix fix = new Fix(line);
+					fixIndex.put(fix.id, fix);
+					footer.setMessage((++count)+": "+fix.id);
+				}
+			}
+			reader.close();
+			
+			cpProgress.println(count + " fixes found");
+			Fix[] fixes = new Fix[fixIndex.size()];
+			fixes = fixIndex.values().toArray(fixes);
+			Arrays.sort(fixes);
+			cpProgress.println("Writing the Fixes.txt file");
+			text = getFixText(fixes);
+			FileUtil.setText(fxxFile, text);
+			
+			uploadAppFiles();
+			
 			cpProgress.println("\nDone.");
 		}
 		catch (Exception ex) {
@@ -209,9 +247,26 @@ public class FAAImporter extends JFrame {
 		chooser.setDialogTitle("Select the FAA 28DaySubscription zip file");
 		chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
 		if (chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
-			return chooser.getSelectedFile();
+			File f = chooser.getSelectedFile();
+			String name = f.getName();
+			int k = name.lastIndexOf("_") + 1;
+			if (k > 0) FileUtil.setText(verFile, name.substring(k, name.length()-4));
+			return f;
 		}
 		else return null;
+	}
+	
+	private void uploadAppFiles() {
+		int option = JOptionPane.showConfirmDialog(null,
+			"Upload app files?", "Upload app files", JOptionPane.YES_NO_OPTION);
+		if (option == JOptionPane.YES_OPTION) {
+			FileUtil.copy(verFile, new File(assets, verFile.getName()));
+			FileUtil.copy(aptFile, new File(assets, aptFile.getName()));
+			FileUtil.copy(seaFile, new File(assets, seaFile.getName()));
+			FileUtil.copy(vorFile, new File(assets, vorFile.getName()));
+			FileUtil.copy(ndbFile, new File(assets, ndbFile.getName()));
+			FileUtil.copy(fxxFile, new File(assets, fxxFile.getName()));
+		}
 	}
 
 	private String getAptText(Airport[] airports, String type) {
@@ -230,6 +285,14 @@ public class FAAImporter extends JFrame {
 			if (nav.type.startsWith(type)) {
 				sb.append(nav.toString());
 			}
+		}
+		return sb.toString();
+	}
+	
+	private String getFixText(Fix[] fixes) {
+		StringBuffer sb = new StringBuffer();
+		for (Fix fix : fixes) {
+			sb.append(fix.toString());
 		}
 		return sb.toString();
 	}
@@ -270,12 +333,6 @@ public class FAAImporter extends JFrame {
 			lon = getLon(line.substring(550, 565));
 			var = line.substring(586,589).trim();
 			if (var.startsWith("0")) var = var.substring(1);
-			if (var.endsWith("W")) {
-				var = "-" + var.substring(0, var.length()-1).trim();
-			}
-			else if (var.endsWith("E")) {
-				var = var.substring(0, var.length()-1).trim();
-			}
 			runways = new LinkedList<Runway>();
 			freqs = new LinkedList<Freq>();
 			String unicom = line.substring(981, 988).trim();
@@ -371,12 +428,14 @@ public class FAAImporter extends JFrame {
 		String use;
 		
 		public Freq(String freq, String use) {
+			freq = freq.replaceAll(";","");
+			use = use.replaceAll(";","");
 			this.freq = freq;
 			this.use = use;
 		}
 		
 		public String toString() {
-			return freq + " - " + use;
+			return freq + " -" + use;
 		}
 	}
 	
@@ -410,12 +469,6 @@ public class FAAImporter extends JFrame {
 			}
 			var = line.substring(481,484).trim();
 			if (var.startsWith("0")) var = var.substring(1);
-			if (var.endsWith("W")) {
-				var = "-" + var.substring(0, var.length()-1).trim();
-			}
-			else if (var.endsWith("E")) {
-				var = var.substring(0, var.length()-1).trim();
-			}
 		}
 		
 		public int compareTo(NAV n) {
@@ -439,8 +492,44 @@ public class FAAImporter extends JFrame {
 		}
 	}
 
+	class Fix implements Comparable<Fix> {
+		String id;
+		String type;
+		String state;
+		String lat;
+		String lon;
+
+		public Fix(String line) {
+			id = line.substring(4,29).trim();
+			type = line.substring(94,97);
+			state = StateCodeTable.getInstance().getCodeForName(line.substring(34,64).trim());
+			lat = getLat(line.substring(66, 80));
+			lon = getLon(line.substring(80, 94));
+		}
+		
+		public int compareTo(Fix n) {
+			return id.compareTo(n.id);
+		}
+		
+		public String toString() {
+			StringBuffer sb = new StringBuffer();
+			sb.append(id + "|");
+			sb.append(type + "|");
+			sb.append("|");
+			sb.append("|");
+			sb.append(state + "|");
+			sb.append(lat + "," + lon + "|");
+			sb.append("|");
+			sb.append("|");
+			sb.append("|");
+			sb.append("\n");
+			return sb.toString();
+		}
+	}
+	
 	//42-08-54.9590N
 	private String getLat(String s) {
+		s = s.trim();
 		int NS = s.length()-1;
 		double deg = Double.parseDouble(s.substring(0,2));
 		double min = Double.parseDouble(s.substring(3,5));
@@ -452,6 +541,7 @@ public class FAAImporter extends JFrame {
 
 	//088-33-43.0095W
 	private String getLon(String s) {
+		s = s.trim();
 		int EW = s.length()-1;
 		double deg = Double.parseDouble(s.substring(0,3));
 		double min = Double.parseDouble(s.substring(4,6));
@@ -525,6 +615,99 @@ public class FAAImporter extends JFrame {
 		}
 		
 		return s;
+	}
+	
+	static class StateCodeTable {
+		static StateCodeTable instance = null;
+		Hashtable<String, String> states = null;
+		protected StateCodeTable() {
+			states = new Hashtable<String, String>();
+			put("Alabama","AL");
+			put("Alaska","AK");
+			put("Alberta","AB");
+			put("American Samoa","AS");
+			put("Arizona","AZ");
+			put("Arkansas","AR");
+			put("Armed Forces (AE)","AE");
+			put("Armed Forces Americas","AA");
+			put("Armed Forces Pacific","AP");
+			put("British Columbia","BC");
+			put("California","CA");
+			put("Colorado","CO");
+			put("Connecticut","CT");
+			put("Delaware","DE");
+			put("District Of Columbia","DC");
+			put("Florida","FL");
+			put("Georgia","GA");
+			put("Guam","GU");
+			put("Hawaii","HI");
+			put("Idaho","ID");
+			put("Illinois","IL");
+			put("Indiana","IN");
+			put("Iowa","IA");
+			put("Kansas","KS");
+			put("Kentucky","KY");
+			put("Louisiana","LA");
+			put("Maine","ME");
+			put("Manitoba","MB");
+			put("Maryland","MD");
+			put("Massachusetts","MA");
+			put("Michigan","MI");
+			put("Minnesota","MN");
+			put("Mississippi","MS");
+			put("Missouri","MO");
+			put("Montana","MT");
+			put("Nebraska","NE");
+			put("Nevada","NV");
+			put("New Brunswick","NB");
+			put("New Hampshire","NH");
+			put("New Jersey","NJ");
+			put("New Mexico","NM");
+			put("New York","NY");
+			put("Newfoundland","NF");
+			put("North Carolina","NC");
+			put("North Dakota","ND");
+			put("Northwest Territories","NT");
+			put("Nova Scotia","NS");
+			put("Nunavut","NU");
+			put("Ohio","OH");
+			put("Oklahoma","OK");
+			put("Ontario","ON");
+			put("Oregon","OR");
+			put("Pennsylvania","PA");
+			put("Prince Edward Island","PE");
+			put("Puerto Rico","PR");
+			put("Quebec","QC");
+			put("Rhode Island","RI");
+			put("Saskatchewan","SK");
+			put("South Carolina","SC");
+			put("South Dakota","SD");
+			put("Tennessee","TN");
+			put("Texas","TX");
+			put("Utah","UT");
+			put("Vermont","VT");
+			put("Virgin Islands","VI");
+			put("Virginia","VA");
+			put("Washington","WA");
+			put("West Virginia","WV");
+			put("Wisconsin","WI");
+			put("Wyoming","WY");
+			put("Yukon Territory","YT");
+		}
+		public static StateCodeTable getInstance() {
+			if (instance == null) {
+				instance = new StateCodeTable();
+			}
+			return instance;
+		}
+		private void put(String name, String code) {
+			states.put(name.toUpperCase(), code);
+		}
+		public String getCodeForName(String name) {
+			String code = states.get(name.toUpperCase());
+			if (code == null) return name;
+			return code;
+		}
 	}
 	
 	void initComponents() {
